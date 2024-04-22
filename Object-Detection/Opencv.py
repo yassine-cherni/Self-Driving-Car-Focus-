7cc601ee48cc8544bd0575aa67303e879936fc04
Yassine Cherni <125700497+yassine-cherni@users.noreply.github.com>
2024-04-22 23:13:38 +0100

    Update and rename Opencv to Opencv.py

---
+++ Object-Detection/Opencv.py
@@ -0,0 +1,15 @@
+import random
+import pandas as pd
+from PIL import Image
+import cv2
+from ultralytics import YOLO
+from IPython.display import Video
+import numpy as np  
+import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set(style='darkgrid')
+import pathlib
+import glob
+from tqdm.notebook import trange, tqdm
+import warnings
+warnings.filterwarnings('ignore')
